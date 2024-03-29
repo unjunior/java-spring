c43292bcad6df54c7e3067fb808cbea2f880d376
Ubirajara Junior <unjunior4315@gmail.com>
2024-03-29 17:17:53 -0300

    criado a classe Product como entidade no banco de dados

---
+++ seguranca/src/main/java/br/com/projeto/seguranca/entities/Product.java
@@ -0,0 +1,33 @@
+package br.com.projeto.seguranca.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    public Product(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
