c0b08475c2cc104e0f2a5c2327fc48a914a0e4ac
Ubirajara Junior <unjunior4315@gmail.com>
2024-04-27 19:26:41 -0300

    ProductMinDTO na busca paginada

---
+++ desafio5-dscommerce-seg/projeto/src/main/java/com/devsuperior/dscommerce/dto/ProductMinDTO.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public class ProductMinDTO {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String imgUrl;
+
+    public ProductMinDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
